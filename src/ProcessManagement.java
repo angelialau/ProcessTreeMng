eab512438ca0044c792143cf20cc3ab6d739248a
angelialau <angelialkm@gmail.com>
2018-02-17 21:03:24 +0800

    echo worksfor stdin and output = filename

--- src/ProcessManagement.java
+++ src/ProcessManagement.java
@@ -3,6 +3,8 @@
  */
 import java.io.*;
 import java.util.*;
+import java.nio.file.*;
+import java.nio.charset.*;
 
 public class ProcessManagement {
 
@@ -41,7 +43,29 @@ public class ProcessManagement {
         for(ProcessGraphNode node: ProcessGraph.nodes){
             if(node.getParents().size() == 0 && node.isRunnable() && !node.isExecuted()) {
                 System.out.println("Running node " + node.getNodeId() + " as a starting node.");
-                processBuilder.command(node.getCommand());
+                if(node.getInputFile().equals(new File("stdin"))){
+                    ArrayList<String> sieved = sieve(node);
+                    String command = sieved.get(0);
+
+                    if(command.equalsIgnoreCase("echo")){
+                        String input = sieved.get(1);
+                        try {
+                            Path path = Paths.get(node.getOutputFile().getAbsolutePath());
+                            Files.write(path, Arrays.asList(input), StandardCharsets.UTF_8,
+                                Files.exists(path) ? StandardOpenOption.APPEND : StandardOpenOption.CREATE);
+                        } catch (IOException ioe) {
+                            System.out.print("Whoops IOException");
+                            ioe.printStackTrace();
+                        }
+                    }
+                    
+                    
+                } 
+                else{
+                    processBuilder.command(node.getCommand());
+                    processBuilder.redirectInput(node.getInputFile());
+                    processBuilder.redirectOutput(node.getOutputFile());
+                }
                 // if(String.valueOf(node.getInputFile()).equalsIgnoreCase("stdin")){
                 //     // Scanner sc = new Scanner(System.in);
                 // } else processBuilder.redirectInput(node.getInputFile());
@@ -76,6 +100,19 @@ public class ProcessManagement {
             //change Executed to true
 
         System.out.println("All process finished successfully");
+    }
+
+    public static ArrayList<String> sieve(ProcessGraphNode node){
+        String[] command = node.getCommand().trim().split(" ");
+        ArrayList<String> output = new ArrayList<>();
+        
+        output.add(command[0]);
+        String input = command[1];
+        for(int i=2; i<command.length; i++){
+            input+= " " + command[i];
+        }
+        output.add(input);
+        return output;
     }
 
 }
--- src/myProg.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class myProg {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Printing the file passed in:");
-        while(sc.hasNextLine()) System.out.println(sc.nextLine());
-    }
-}
