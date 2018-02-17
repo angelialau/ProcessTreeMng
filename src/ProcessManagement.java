df1279c7f12c1a05d30f5396a192fc3a7041dd12
angelialau <angelialkm@gmail.com>
2018-02-17 16:40:09 +0800

    happy cny i have no idea how to start process and do stdin and outs

--- src/ProcessManagement.java
+++ src/ProcessManagement.java
@@ -1,7 +1,7 @@
 /**
  * Created by jit_biswas on 2/1/2018.
  */
-import java.io.File;
+import java.io.*;
 import java.util.*;
 
 public class ProcessManagement {
@@ -16,31 +16,57 @@ public class ProcessManagement {
 
         //parse the instruction file and construct a data structure, stored inside ProcessGraph class
         ParseFile.generateGraph(new File(args[0]));
+        ProcessBuilder processBuilder = new ProcessBuilder();
 
         // Print the graph information
         // WRITE YOUR CODE
         // ProcessGraph.printGraph(); // prints detailed ProcessGraph
         // ProcessGraph.printBasic();
-        Stack<ArrayList<Integer>> stack = new Stack<>();
-        
-
-        // Using index of ProcessGraph, loop through each ProcessGraphNode, to check whether it is ready to run
-        // check if all the nodes are executed
-        // WRITE YOUR CODE
-        ArrayList<Integer> terminalNodes = new ArrayList<>();
-        for(ProcessGraphNode node: ProcessGraph.nodes){
-            if(node.getChildren().size() == 0) terminalNodes.add(node.getNodeId());
-            System.out.print("child node: " + node.getNodeId() + " ");
+        for(ProcessGraphNode node : ProcessGraph.nodes){
+            System.out.print("node: " + node.getNodeId() + " ");
             System.out.print("parent nodes: ");
             for (ProcessGraphNode childNode : node.getParents())
                 System.out.print(" " + childNode.getNodeId());
             System.out.print(" \n");
+            System.out.print("child nodes: ");
+            for (ProcessGraphNode childNode : node.getChildren())
+                System.out.print(" " + childNode.getNodeId());
+            System.out.print(" \n");
         }
-        stack.push(terminalNodes);
 
-        for(Integer nodeIndex : stack.peek()){ // end nodes 
+        // Using index of ProcessGraph, loop through each ProcessGraphNode, to check whether it is ready to run
+        // check if all the nodes are executed
+        // WRITE YOUR CODE
+        
+        for(ProcessGraphNode node: ProcessGraph.nodes){
+            if(node.getParents().size() == 0 && node.isRunnable() && !node.isExecuted()) {
+                System.out.println("Running node " + node.getNodeId() + " as a starting node.");
+                processBuilder.command(node.getCommand());
+                // if(String.valueOf(node.getInputFile()).equalsIgnoreCase("stdin")){
+                //     // Scanner sc = new Scanner(System.in);
+                // } else processBuilder.redirectInput(node.getInputFile());
+                // if(String.valueOf(node.getOutputFile()).equalsIgnoreCase("stdout")){
+                //     processBuilder.redirectOutput();
+                // } else processBuilder.redirectOutput(node.getOutputFile());
+                
+                try{
+                    Process process = processBuilder.start();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                    for (String line; (line = bufferedReader.readLine()) != null;) {
+                        System.out.println(line);  }
+                    bufferedReader.close();
+                }catch(Exception e){
+                    System.out.println(e.getMessage());
+                }
 
-        }
+                node.setExecuted();
+                System.out.println("Finished executing process/node " + node.getNodeId());
+
+            } else{
+                
+            }
+            
+        } 
 
         //mark all the runnable nodes
         // WRITE YOUR CODE
---
+++ src/myProg.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class myProg {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Printing the file passed in:");
+        while(sc.hasNextLine()) System.out.println(sc.nextLine());
+    }
+}
